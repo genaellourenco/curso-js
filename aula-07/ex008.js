efbcefb6b82ef3d18fa0470d59809c5c75372a41
Genael Lourenço Filho <108880124+genaellourenco@users.noreply.github.com>
2022-08-14 21:17:03 -0300

    atualizei

---
+++ aula-07/ex008.js
@@ -0,0 +1,8 @@
+
+
+var vel = 60 
+console.log(`A velocidade do carro é ${vel} km/h`)
+if(vel > 60){
+    console.log('Velocidade acima da permitida, MULTADO!!')
+}
+console.log('Dirija sempre com o cinto de segurança! Boa Viagem!')
