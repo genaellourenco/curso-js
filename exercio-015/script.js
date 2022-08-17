a644f99a4e9e1849d97ad108487a20e6e5f639c4
Genael Lourenço Filho <108880124+genaellourenco@users.noreply.github.com>
2022-08-17 15:34:01 -0300

    atz

---
+++ exercio-015/script.js
@@ -0,0 +1,19 @@
+function carregar(){
+    
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas!`
+    // if(hora >= 0 && hora < 12) {
+    //     // BOM DIA
+    //     img.src = 'bomdia.jpg'
+    // } else if ( hora >= 12 && hora < 18) {
+    //     // BOA TARDE
+    //     img.src = 'boatarde.jpg'
+    // } else {
+    //     // BOA NOITE
+    //     img.src = 'boanoite.jpg'
+    // }
+
+}
