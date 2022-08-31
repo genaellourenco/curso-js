f1d4e0c71749f34cc690b4df0d7acb7265213f81
Genael Lourenço Filho <108880124+genaellourenco@users.noreply.github.com>
2022-08-31 15:44:05 -0300

    atz

---
+++ aula-13/ex-01.js
@@ -0,0 +1,22 @@
+// var c = 1
+// while( c <= 10){
+//     console.log(`Passo ${c}`)
+//     c++
+// }
+
+
+// var c = 1 
+
+// do{
+
+// console.log(`Passo ${c}`)
+// c++
+
+// } while( c <= 4)
+
+console.log('Inicio....')
+for(var c=1;c<=4;c++){
+    
+    console.log(c)
+}
+console.log('....Fim')
