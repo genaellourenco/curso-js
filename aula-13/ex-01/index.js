98e55a0c3b0851e3928d25b04143e1dcf8af87cd
Genael Lourenço Filho <108880124+genaellourenco@users.noreply.github.com>
2022-09-02 07:45:17 -0300

    atz

---
+++ aula-13/ex-01/index.js
@@ -0,0 +1,21 @@
+
+function contar(){
+
+    let inicio = document.getElementById('inicio')
+    let fim = document.getElementById('fim')
+    let pular = document.getElementById('pular')
+    let res = document.getElementById('res') 
+
+    if(inicio.value.length == 0 || fim.value.length == 0 || pular.value.length == 0){
+        alert('[ERRO] Falta dados !')
+    } else {
+        res.innerHTML = "Contando..."
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(pular.value)
+
+        for(let c=)
+    }
+    
+}
+
